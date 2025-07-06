3d50cda672ec97bf0fc504c2991d1275e87d28c5
Bill Njoroge <williamsriunge@gmail.com>
2025-07-05 23:31:33 -0400

    FIX: Correct arguments for leadership candidacy

--- protocol/device_classes.py
+++ protocol/device_classes.py
@@ -533,7 +533,7 @@ class ThisDevice(Device):
             checkin_msg = Message(
                 action=Action.CHECK_IN.value, payload=0, leader_id=self.id, follower_id=id
             )
-            await self.transceiver.async_send(id, checkin_msg)
+            await self.transceiver.async_send(id, checkin_msg.msg)
             # device hangs in send() until finished sending
             end_time = time.time() + RESPONSE_ALLOWANCE
             # accounts for leader receiving another device's check-in response (which should never happen)
