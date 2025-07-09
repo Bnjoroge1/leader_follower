fc4ab8138497651898a13ab07b279e22849f0f2f
Bill Njoroge <williamsriunge@gmail.com>
2025-07-09 18:25:33 -0400

    nework

--- protocol/vm_network.py
+++ protocol/vm_network.py
@@ -24,7 +24,7 @@ class VMNetworkUDP(asyncio.DatagramProtocol):
           self.transport = transport
           print("UDP endpoint ready and lisening")
      def datagram_received(self, data: bytes, addr: tuple[str | Any, int]) -> None:
-          print(f"received {bytes.decode("utf-8")}")
+          print(f"received {bytes.decode(encoding="utf-8")}")
           self.receive_queue.put_nowait(data)
      def connection_lost(self, exc: Exception | None) -> None:
           self.transport.close()
